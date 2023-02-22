b93ade2c6b7ef97a2b1c2eea0c04d4d63405918c
leolima <leolimaasantos@gmail.com>
2023-02-22 14:57:08 -0300

    enhancing output

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let button = window.document.querySelector('button#calculateButton');
 let checkbox = window.document.querySelector('input#unilateralSwitch');
+let result = window.document.querySelector('div#result');
 
 button.addEventListener('click', mainFunction);
 
@@ -8,29 +9,42 @@ function mainFunction() {
     let totalWeight = Number(totalWeightText.value);
     let mainWeight
 
-    if (checkbox.checked == false){
-    mainWeight = (totalWeight / 2);
+    if (totalWeight > 0) {
+        if (checkbox.checked == false){
+        mainWeight = (totalWeight / 2);
+        } else {
+        mainWeight = (totalWeight);
+        }
+
+        function calculator(plateWeight) {     
+            
+            let plateAmount = Math.floor(mainWeight / plateWeight);
+            mainWeight %= plateWeight;
+            return plateAmount;
+        }
+
+        let amountTwenty = calculator(20);
+        let amountFifteen = calculator(15);
+        let amountTen = calculator(10);
+        let amountFive = calculator(5);
+        let amountTwoAndAHalf = calculator(2.5);
+
+        let isRemaining
+
+        if (mainWeight > 0) {
+            isRemaining = `<br> Remaining weight: ${checkbox.checked ? mainWeight : mainWeight*2}kg`
+        } else {
+            isRemaining = ''
+        }
+        
+        result.innerHTML =
+        `20 kg plates: ${amountTwenty}<br>
+        15 kg plates: ${amountFifteen}<br>
+        10 kg plates: ${amountTen}<br>
+        05 kg plates: ${amountFive}<br>
+        2.5 kg plates: ${amountTwoAndAHalf}
+        ${isRemaining}`;
     } else {
-    mainWeight = (totalWeight);
+        result.innerHTML = `Please input a valid number`
     }
-
-    function calculator(plateWeight) {
-        let plateAmount = Math.floor(mainWeight / plateWeight);
-        mainWeight %= plateWeight;
-        return plateAmount;
-    }
-
-    let amountTwenty = calculator(20);
-    let amountFifteen = calculator(15);
-    let amountTen = calculator(10);
-    let amountFive = calculator(5);
-    let amountTwoAndAHalf = calculator(2.5);
-
-    let result = window.document.querySelector('div#result');
-    result.innerHTML =
-    `20 kg plates: ${amountTwenty}<br>
-    15 kg plates: ${amountFifteen}<br>
-    10 kg plates: ${amountTen}<br>
-    05 kg plates: ${amountFive}<br>
-    2.5 kg plates: ${amountTwoAndAHalf}`;
 }
